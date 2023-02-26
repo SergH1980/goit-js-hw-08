c389d89355c7cc73687bc21701896f1eb26a2a7b
SergH1980 <serg.galaka@gmail.com>
2023-02-27 00:04:13 +0200

    task 2 completed, problem with throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+import { throttle } from 'throttle-debounce';
+console.log(throttle);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const timeUpdate = player.on(
+  `timeupdate`,
+  throttle(1000, event => {
+    localStorage.setItem(CURRENT_TIME_KEY, event.seconds);
+    console.log(event.seconds);
+  })
+);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const timeWatched = localStorage.getItem(CURRENT_TIME_KEY);
+
+if (timeWatched) {
+  player.setCurrentTime(`${timeWatched}`);
+}
